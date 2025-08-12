46c186395683f969b1148e689f41785411ce3be4
lucas021810 <lucasarandav02@gmail.com>
2025-08-12 10:25:17 -0700

    Add files via upload

--- Initials/src/Main.java
+++ Initials/src/Main.java
@@ -1,15 +1,15 @@
 import kareltherobot.*;
- 
+
 public class Main implements Directions{
   public static void main(String[] args) {
     World.setVisible(true);// allows us to see the run
 
     // The line below creates a Robot that we will refer to as r.  
     // Find out what the numbers and direction do!
-    Robot r = new Robot(8,1,South,20);
+    Robot r = new Robot(8,1,South,28);
 		
     // examples of commands you can invoke on a Robot
-    r.putBeeper();  // move one step in the direction it is facing
+   r.putBeeper();  // move one step in the direction it is facing
     r.move();
     r.putBeeper();
     r.move();
@@ -40,7 +40,43 @@ public class Main implements Directions{
     r.move();
     r.putBeeper();
     r.move();
-    
-
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();  
   }
 }
